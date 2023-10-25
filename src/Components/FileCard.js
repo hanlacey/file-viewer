ee09c8e3df90c683a55f4b72430ea950d47a0069
Hannah Lacey <hannahl@gendius.co.uk>
2023-10-25 16:15:41 +0100

    update styling

--- src/Components/FileCard.js
+++ src/Components/FileCard.js
@@ -8,19 +8,42 @@ const Card = styled.div`
     justify-content: center;
     height: 10em;
     width: 10em;
-    border-radius: 10px;
+    border-radius: 6px;
     margin: 10px;
     background: #f0f2f2;
     box-shadow: 0px 0px 12px -14px rgba(0,0,0,1);
-    color:#00779e
+    color: #7489a6
+`
+const Thumbnail = styled.div`
+    display:flex;
+    align-items: center;
+    justify-content:center;
+    background: #6e9ce6;
+    color: white;
+    height: 30%;
+    width: 100%;
+    border-top-right-radius: 6px;
+    border-top-left-radius: 6px;
+    font-size: 20px
+`
+
+const Description = styled.div`
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content:space-evenly;
+    height: 70%;
 `
 
 function FileCard({ item }) {
     return (
         <Card >
-            <h3 data-testid="file-card-name" >{item.name}</h3>
-            <div>{item.type}</div>
-            <div>Added: {item.added}</div>
+            <Thumbnail>{item.type}</Thumbnail>
+            <Description>
+                <div data-testid="file-card-name">{item.name}</div>
+                <div>Added: {item.added}</div>
+            </Description>
+
         </Card>)
 }
 
