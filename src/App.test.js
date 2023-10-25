80ba29cb83b8be13a170d7f3c1698807583a434b
Hannah Lacey <hannahl@gendius.co.uk>
2023-10-25 14:55:57 +0100

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('App renders component with buttons', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Name')).toBeInTheDocument();
+  expect(screen.getByText('Most Recent')).toBeInTheDocument();
+  expect(screen.getByText('Type')).toBeInTheDocument();
+});
+
+test('renders files and folders', () => {
+  render(<App />);
+  expect(screen.getByText('Employee Handbook')).toBeInTheDocument();
+  expect(screen.getByText('Expenses')).toBeInTheDocument();
+});
+
+test('button click changes sorting criteria and updates button styling', () => {
+  render(<App />);
+  const recentButton = screen.getByText('Most Recent');
+  const nameButton = screen.getByText('Name');
+
+  fireEvent.click(recentButton);
+
+  expect(recentButton).toHaveStyle('background-color: #91a7c9');
+  expect(nameButton).not.toHaveStyle('background-color: #91a7c9');
 });
